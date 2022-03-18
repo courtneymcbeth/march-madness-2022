2efba5c05208225a98b2a677f9bd4084edd22c8b
Courtney McBeth <clm325@cornell.edu>
2022-03-18 11:58:40 -0400

    debugging

--- js/master.js
+++ js/master.js
@@ -2,7 +2,7 @@ var links = document.getElementsByClassName("player_link_inside");
 
 const url = window.location.href;
 
-for (var link of link) {
+for (var link of links) {
   const name = link.innerHTML;
   const name_lc = name.toLowerCase();
   if (url.includes(name_lc)) {
