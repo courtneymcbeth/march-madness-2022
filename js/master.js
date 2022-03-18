1efcdda7a7ad36add08951bbaa914a5722bd62c0
Courtney McBeth <clm325@cornell.edu>
2022-03-18 12:33:29 -0400

    debugging

--- js/master.js
+++ js/master.js
@@ -1,15 +1,11 @@
 var links = document.getElementsByClassName("player_link_inside");
 
 const url = window.location.href.split('/');
-console.log(url);
 
 for (var link of links) {
   const name = link.innerHTML;
-  console.log(name);
   const name_lc = name.toLowerCase();
-  console.log(name_lc);
   if (url[url.length - 2].includes(name_lc)) {
     link.style.color = "#005eb8";
-    console.log(link.style);
   }
 }
