8ac7e5b93371648e3bdfc367b05fa7522c29f8c2
Courtney McBeth <clm325@cornell.edu>
2022-03-18 11:56:43 -0400

    adding js

---
+++ js/master.js
@@ -0,0 +1,11 @@
+var links = document.getElementsByClassName("player_link_inside");
+
+const url = window.location.href;
+
+for (var link of link) {
+  const name = link.innerHTML;
+  const name_lc = name.toLowerCase();
+  if (url.includes(name_lc)) {
+    link.innerHTML = '<b>' + link.innerHTML + '</b>';
+  }
+}
